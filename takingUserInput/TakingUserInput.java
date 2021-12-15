06a993401ae94016ca0c5178b83c72d386f0b900
vivek napit <viveknapit12@gmail.com>
2021-12-15 20:07:58 +0530

    Operators added

---
+++ takingUserInput/TakingUserInput.java
@@ -0,0 +1,64 @@
+package takingUserInput;
+import java.util.Scanner;
+
+public class TakingUserInput {
+
+	
+		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		String a = sc.nextLine();
+//		String b = sc.nextLine();
+//		String c = sc.nextLine();
+//		String d = sc.nextLine();
+//		String e = sc.nextLine();
+//		
+//		String f = c;
+//		
+//		System.out.println("you best friend is " + f);
+//		
+		
+//		public static void main(String[] args) {
+//			
+//			int a = 10;
+//			int v = 30;
+//			int f = 40;
+//			float g = 4.4f;
+//			double j = 4.4;
+//			
+//			
+//			double  x = (a+v) - (f+g) / j;
+//			
+//			System.out.println(x);
+	
+//	public static void main(String[] args) {
+//		
+//		String myIntroduction = "hello, my self vivek from rewa. ";
+//		
+//		String myLearning = " In these days I am learning java programming langauge."
+//				+ " And I am very for learning this.";
+//		
+//		String result = myIntroduction + myLearning;
+//		
+//		System.out.println(" tell more about yourself ?" 
+//				+ result);
+//		
+//		
+//		int a = 4;
+//		int b = 4;
+//		
+//		int c = a-b;
+//		
+//		System.out.println(c);
+//		
+		
+	 public static void main(String[] args) {
+		 
+		 Scanner sc = new Scanner(System.in);
+		 
+		 
+		
+		
+	}
+
+}

ca664f405295099dd70f73aeea86d6814dafc5bc
vivek napit <viveknapit12@gmail.com>
2021-12-15 20:03:30 +0530

    DataType file added

---
+++ DataType/DataTypeTutorial.java
@@ -0,0 +1,23 @@
+package DataType;
+
+public class DataTypeTutorial {
+
+	public static void main(String[] args) {
+		
+		int age = 10;
+		
+		float rateOfInterest = 2.3f;
+		
+		double rate = 3.1428571;
+		
+		
+		boolean isThisSeriesCool = false;
+		
+		
+		char mycharacter = '@';
+		
+		System.out.println(isThisSeriesCool);
+		
+	}
+
+}
---
+++ DataType/TypeCasting.java
@@ -0,0 +1,16 @@
+package DataType;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		
+		double myDouble = 2.6548;
+		
+		int myInt =(int) myDouble;
+		
+		System.out.println(myInt);
+
+
+	}
+
+}
